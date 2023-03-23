c24686af3dac121b953a4e2ebdb31e1dfdd3e2af
Marquitos <marcospborborema@hotmail.com>
2023-03-22 21:09:34 -0300

    Atualização do Dado

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -27,8 +28,19 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val DiceImage: ImageView = findViewById(R.id.imageView3)
+       val drawableResource = when  (diceRoll) {
+           1 -> (R.drawable.dice_1)
+           2 -> (R.drawable.dice_2)
+           3 -> (R.drawable.dice_3)
+           4 -> (R.drawable.dice_4)
+           5 -> (R.drawable.dice_5)
+           else -> (R.drawable.dice_6)
+
+
+       }
+        DiceImage.setImageResource(drawableResource)
+        DiceImage.contentDescription = diceRoll.toString()
     }
 
     class Dice(private val numSides: Int) {
